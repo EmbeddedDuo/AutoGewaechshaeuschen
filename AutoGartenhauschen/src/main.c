2c1f8ac9d76a961162cd2eb2b88471c0a5f00dd3
Philipp Drüke <philipp.drueke@informatik.hs-fulda.de>
2024-01-24 18:58:06 +0100

    removed test tasks

--- AutoGartenhauschen/src/main.c
+++ AutoGartenhauschen/src/main.c
@@ -126,52 +126,6 @@ void lcd_task(void *pvParameters)
     }
 }
 
-/*
-void lcd_test(void *pvParameters)
-{
-    hd44780_t lcd = {
-        .write_cb = write_lcd_data, // use callback to send data to LCD by I2C GPIO expander
-        .font = HD44780_FONT_5X8,
-        .lines = 2,
-        .pins = {
-            .rs = 0,
-            .e = 2,
-            .d4 = 4,
-            .d5 = 5,
-            .d6 = 6,
-            .d7 = 7,
-            .bl = 3}};
-
-    memset(&pcf8574, 0, sizeof(i2c_dev_t));
-    ESP_ERROR_CHECK(pcf8574_init_desc(&pcf8574, CONFIG_EXAMPLE_I2C_ADDR, 0, CONFIG_EXAMPLE_I2C_MASTER_SDA, CONFIG_EXAMPLE_I2C_MASTER_SCL));
-
-    ESP_ERROR_CHECK(hd44780_init(&lcd));
-
-    hd44780_switch_backlight(&lcd, true);
-
-    hd44780_upload_character(&lcd, 0, char_data);
-    hd44780_upload_character(&lcd, 1, char_data + 8);
-
-    hd44780_gotoxy(&lcd, 0, 0);
-    hd44780_puts(&lcd, "\x08 Hello world!");
-    hd44780_gotoxy(&lcd, 0, 1);
-    hd44780_puts(&lcd, "\x09 ");
-
-    char time[16];
-
-    while (1)
-    {
-        hd44780_gotoxy(&lcd, 2, 1);
-
-        snprintf(time, 7, "%" PRIu32 "  ", get_time_sec());
-        time[sizeof(time) - 1] = 0;
-
-        hd44780_puts(&lcd, time);
-
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-} */
-
 void dht_task(void *pvParameters)
 {
     uint8_t *dht_gpio_ptr = (uint8_t *)pvParameters;
@@ -214,28 +168,7 @@ void dht_task(void *pvParameters)
     }
 }
 
-/*
-void dht_test(void *pvParameters)
-{
-    uint8_t *dht_gpio_ptr = (uint8_t *)pvParameters;
-    uint8_t dht_gpio = *dht_gpio_ptr;
-    printf("Der Pin lautet: %" PRId8 "\n", dht_gpio);
 
-    float temperature, humidity;
-
-    while (1)
-    {
-        if (dht_read_float_data(SENSOR_TYPE, dht_gpio, &humidity, &temperature) == ESP_OK)
-            printf("Humidity: %.1f%% Temp: %.1fC an Pin %" PRId8 "\n", humidity, temperature, dht_gpio);
-        else
-            printf("Could not read data from sensor\n");
-
-        // If you read the sensor data too often, it will heat up
-        // http://www.kandrsmith.org/RJS/Misc/Hygrometers/dht_sht_how_fast.html
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
-}
-*/
 void photoresistor_test()
 {
 
@@ -335,43 +268,6 @@ void servo_task()
     }
 }
 
-/*
-
-void servo_test()
-{
-
-    servo_config_t servo_cfg = {
-        .max_angle = 180,
-        .min_width_us = 500,
-        .max_width_us = 2500,
-        .freq = 50,
-        .timer_number = LEDC_TIMER_0,
-        .channels = {
-            .servo_pin = {
-                SERVO_CH1_PIN},
-            .ch = {
-                LEDC_CHANNEL_1,
-            },
-        },
-        .channel_number = 1,
-    };
-
-    while (true)
-    {
-        ESP_ERROR_CHECK(iot_servo_init(LEDC_LOW_SPEED_MODE, &servo_cfg));
-        size_t i;
-        float angle_ls;
-        for (i = 0; i <= 180; i++)
-        {
-            iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 1, i);
-            vTaskDelay(50 / portTICK_PERIOD_MS);
-            iot_servo_read_angle(LEDC_LOW_SPEED_MODE, 0, &angle_ls);
-            ESP_LOGI("servo", "[%d|%.2f] ", i, angle_ls);
-        }
-
-        iot_servo_deinit(LEDC_LOW_SPEED_MODE);
-    }
-} */
 
 void app_main()
 {
@@ -403,13 +299,6 @@ void app_main()
     {
         ESP_LOGI("Queue2", "windowState successfully sent");
     }
-    
-
-    /*
-    xTaskCreate(dht_test, "dht_pin1", configMINIMAL_STACK_SIZE * 3, &dht_gpio_1, 5, NULL);
-    xTaskCreate(dht_test, "dht_pin2", configMINIMAL_STACK_SIZE * 3, &dht_gpio_2, 5, NULL);
-    xTaskCreate(dht_test, "dht_pin3", configMINIMAL_STACK_SIZE * 3, &dht_gpio_3, 5, NULL);
-    */
     
     ESP_ERROR_CHECK(i2cdev_init());
     xTaskCreate(lcd_task, "lcd_task", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
