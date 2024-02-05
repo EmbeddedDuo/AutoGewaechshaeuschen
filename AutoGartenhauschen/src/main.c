e7d17138ee3fb3cc2bcee7641d1877077a98d242
Philipp Drüke <philipp.drueke@informatik.hs-fulda.de>
2024-02-05 01:44:12 +0100

    "deleted nvs_flash init"

--- AutoGartenhauschen/src/main.c
+++ AutoGartenhauschen/src/main.c
@@ -537,16 +537,6 @@ httpd_handle_t start_webserver(void)
 
 void app_main()
 {
-    esp_err_t ret = nvs_flash_init(); // Initialize NVS Flash memory for the WIFI credentials
-
-    // Check for NVS initialization errors
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret); // Check for general initialization errors
-
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     init_wifi(); // Initialize Wi-Fi
